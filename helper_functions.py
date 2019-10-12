f72df8eaf158fd93216c339cf7af180b32c4ac91
Michael Hoyer <michael.hoyer3@gmail.com>
2019-10-12 13:50:22 +0200

    Clean up

---
+++ helper_functions.py
@@ -0,0 +1,75 @@
+import math
+
+TAKEOFF_HEIGHT = 1
+OPERATING_HEIGHT = 10
+LANDING_HEIGHT = 20
+RADIUS_AROUND_WINDMILL = 20
+
+class Super_point:
+    x = 0
+    y = 0
+    z = 0
+    yaw = 0
+    def __init__(self, x, y, yaw):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+
+def is_at_target(drone):
+    distance_to_target = ((drone.target.x - drone.position.x)**2 +
+                          (drone.target.y - drone.position.y)**2 +
+                          (drone.target.z - drone.position.z)**2)**0.5
+    velocity = (drone.velocity.x**2 + drone.velocity.y**2 + drone.velocity.z**2)**0.5
+
+    if distance_to_target < 0.5 and velocity < 0.2:
+        return True
+
+    return False
+    
+
+# Returnerer jaw mot windmøllen
+def yaw_towards_windmill(drone_pos, windmill_pos):
+    angle = angle3pt((windmill_pos.x, windmill_pos.y + 1), (windmill_pos.x, windmill_pos.y), (drone_pos.x, drone_pos.y))-90
+    yaw = math.radians(angle)
+    return yaw
+
+
+def get_closer_target(drone, target, distance=10):
+    diff = (drone.position.x - target.x, drone.position.y - target.y)
+    lenght = (diff[0]**2 + diff[1]**2)**0.5
+    unit_diff = (diff[0] / lenght, diff[1] / lenght)
+    dx = unit_diff[0] * distance
+    dy = unit_diff[1] * distance
+    return (target.x + dx, target.y + dy)
+
+
+def angle3pt(a, b, c):
+    """Counterclockwise angle in degrees by turning from a to c around b
+        Returns a float between 0.0 and 360.0"""
+    ang = math.degrees(
+        math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
+    return ang + 360.0 if ang < 0.0 else ang
+
+## Returnerer en liste med points(x, y, yaw) med lenght lik total_points. Maa kjøres i første bildeposisjon.
+def points_around_windmill(drone, windmill_pos): # need: import math
+    radius = RADIUS_AROUND_WINDMILL          # Avstand fra fyrtårn [m]
+    total_points = 3    # Antall punkter rundt fyrtårnet. Funker med 1, 2, 3, 4, 6, 12.
+    t = list((range(0, 360, int(360/12))))
+    point = []
+    x = []
+    y = []
+    for i in range(len(t)):  # Lager 12 punkter med radius radius fra origo
+        x.append(radius*(math.sin(math.radians(t[i]))))
+        y.append(radius*(math.cos(math.radians(t[i]))))
+    index_array = list((range(int(12/total_points), 12, int(12/total_points))))  # Første punkt er drone_pos!! (evt endre på første )
+     # Første punkt er drone_pos!!
+     # Finner vinkel for verdenskoordinater til dronekoordinater
+    angle = angle3pt((windmill_pos.x, windmill_pos.y + radius), (windmill_pos.x, windmill_pos.y), (drone.position.x, drone.position.y))
+    point.append(Super_point(drone.position.x, drone.position.y, math.radians(angle-90)))  # Første punkt er drone_pos!!
+    for i in index_array:
+        point.append(Super_point((x[i]*math.cos(math.radians(angle)) - y[i]*math.sin(math.radians(angle))) + windmill_pos.x, (y[i]*math.cos(math.radians(angle)) + x[i]*math.sin(math.radians(angle))) + windmill_pos.y, math.radians(angle3pt((windmill_pos.x, windmill_pos.y + radius), (windmill_pos.x, windmill_pos.y), ((x[i]*math.cos(math.radians(angle)) - y[i]*math.sin(math.radians(angle))) + windmill_pos.x, (y[i]*math.cos(math.radians(angle)) + x[i]*math.sin(math.radians(angle))) + windmill_pos.y))-90)))
+    return point
+
+
+def score(report, score_dict):
+    return score_dict[(report.position.x, report.position.y)]
--- main.py
+++ main.py
@@ -14,22 +14,10 @@ from is_rust import is_rust
 from rust_score import rust_score
 import make_path
 from save_photos import save_photos
+from helper_functions import *
 
-
-TAKEOFF_HEIGHT = 1
-OPERATING_HEIGHT = 10
-LANDING_HEIGHT = 20
-RADIUS_AROUND_WINDMILL = 20
-
-class Super_point:
-    x = 0
-    y = 0
-    z = 0
-    yaw = 0
-    def __init__(self, x, y, yaw):
-        self.x = x
-        self.y = y
-        self.yaw = yaw
+TASK2 = True
+RUST_THRESHOLD = 20
 
 
 class Starting_mission(smach.State):
@@ -68,10 +56,13 @@ class Flying_to_target(smach.State):
         if target.x == 0 and target.y == 0: # target is launch pad
             userdata.drone.set_target(target.x, target.y, LANDING_HEIGHT)
         else:
-            # Scare target such that drone stops in front of windmill
+            # Modify target such that drone stops in front of windmill
             userdata.current_windmill = target
             x_new, y_new = get_closer_target(userdata.drone, target, RADIUS_AROUND_WINDMILL)
+
+            # find desired yaw
             target_yaw = yaw_towards_windmill(Super_point(x_new, y_new, 0), target)
+            
             userdata.drone.set_target(x_new, y_new, OPERATING_HEIGHT, yaw = target_yaw)
 
         # TODO: collision avoidance
@@ -79,7 +70,7 @@ class Flying_to_target(smach.State):
         while not is_at_target(userdata.drone):
             continue
 
-        if not userdata.path: # no more targets in path => drone is back home
+        if not userdata.path:   # no more targets in path => drone is back home
             return 'arrived_at_landing_pos'
         else:
             return 'arrived_at_windmill'
@@ -95,65 +86,58 @@ class Inspecting(smach.State):
     def execute(self, userdata):
         sub_path = points_around_windmill(userdata.drone, userdata.current_windmill)
         images = []
+        has_rust = False
+        rust_images = []
 
 
         for target in sub_path:
 
             # Fly to target
-            print("Yaw 1")
-            print(math.degrees(target.yaw))
             userdata.drone.set_target(target.x, target.y, OPERATING_HEIGHT, yaw=target.yaw)
 
             while not is_at_target(userdata.drone) :
                 continue
 
-            '''
-            print("is_at_yaw:")
-            yaw_diff = abs(userdata.drone.yaw - userdata.drone._setpoint_msg.yaw)
-            print(userdata.drone.yaw)
-            print(userdata.drone._setpoint_msg.yaw)
-            print(yaw_diff)
-
-
-            while not is_at_yaw(userdata.drone):
-                continue
-            '''
-
             # Take and analyse photo
-            print("Yaw 2")
-            print(math.degrees(userdata.drone.yaw))
-            images.append(userdata.drone.camera.image)
+            if TASK2:
+                img = userdata.drone.camera.image
+                if rust_score(img) > RUST_THRESHOLD:
+                    # not necessary to check windmill further
+                    has_rust = True
+                    rust_report = build_rust_report_message(userdata.current_windmill, has_rust, rust_images)
+                    rospy.loginfo("Sending rust report for task 2")
+                    send_single_inspection_report(rust_report)
+                    return 'inspection_complete'
+
+            else:
+                images.append(userdata.drone.camera.image)
 
 
-        # Evaluate if/how much rust on windmill
-        RUST_THRESHOLD = 50
+        if TASK2:
+            rust_report = build_rust_report_message(userdata.current_windmill, has_rust, rust_images)
+            rospy.loginfo("Sending rust report for task 2")
+            send_single_inspection_report(rust_report)
+        
+        else:
+            photo_id = 0
+            for img in images:
+                score = rust_score(img)
 
-        has_rust = False
-        rust_images = []
-        photo_id = 0
-        for img in images:
-            score = rust_score(img)
+                # Lagre bilde og score
+                windmill_position = userdata.current_windmill
+                save_photos(windmill_position, photo_id, img, score)
+                photo_id = photo_id + 1
 
-            # Lagre bilde og score
-            windmill_position = userdata.current_windmill
-            save_photos(windmill_position, photo_id, img, score)
-            photo_id = photo_id + 1
+                if score > RUST_THRESHOLD:
+                    has_rust = True
+                    rust_images.append(img)
+                
+                # Save rust score for later sorting
+                userdata.rust_score_dict[(windmill_position.x, windmill_position.y)] = rust_score
 
-            if score > RUST_THRESHOLD:
-                has_rust = True
-                rust_images.append(img)
+                # Save report for current windmill
+                userdata.rust_reports.append(build_rust_report_message(windmill_position, has_rust, rust_images))
 
-        # TASK 3 REPORTING
-        # Save rust score for later sorting
-        #userdata.rust_score_dict[(current_windmill.x, current_windmill.y)] = rust_score
-
-        # Save report for current windmill
-        #userdata.rust_reports.append(build_rust_report_message(userdata.current_windmill, has_rust, rust_images))
-
-        # TASK 2 REPORTING 
-        rust_report = build_rust_report_message(userdata.current_windmill, has_rust, rust_images)
-        rospy.loginfo("Sending rust report for task 2")
-        send_single_inspection_report(rust_report)
 
         return 'inspection_complete'
 
@@ -168,116 +152,18 @@ class Ending_mission(smach.State):
     def execute(self, userdata):
         userdata.drone.land()
 
-        # TASK 3 REPORTING
-        # sorts reports based on return value from function score
-        # reports = userdata.rust_reports
-        # sortet_rust_reports = sorted(reports, key=lambda report: score(report, userdata.rust_score_dict))
-        # rospy.loginfo("Sending rust reports for task 3")
-        # send_total_inspection_report(sortet_rust_reports)
+        if not TASK2:
+            # sorts reports based on return value from function score
+            reports = userdata.rust_reports
+            sortet_rust_reports = sorted(reports, key=lambda report: score(report, userdata.rust_score_dict))
+            rospy.loginfo("Sending rust reports for task 3")
+            send_total_inspection_report(sortet_rust_reports)
 
         while (abs(userdata.drone.velocity.z) > 0.8):
             continue
         userdata.drone.deactivate()
 
         return 'mission_ended'
-
-
-
-
-
-'''
-def make_path(points):
-    distances = []
-    for i in range(0, len(points)):
-        sub_distances = []
-        for j in range(0, len(points)):
-            distance = ((points[j].x - points[i].x)**2 + (points[j].y - points[i].y)**2)**0.5
-            sub_distances.append(distance)
-        distances.append(sub_distances)
-
-    last_index = len(points) - 1
-    path_indexes = solve_tsp(distances, endpoints = (0,last_index) )
-
-    path = []
-    for i in range(1, len(points)):
-        index = path_indexes[i-1]
-        path.append(points[index])
-
-    return path
-'''
-
-def analyse_photo(img):   # TODO: implement photo taking and analysis
-    """
-    Returns a score for how much rust there is in the image
-    """
-    pass
-
-def is_at_target(drone):
-    distance_to_target = ((drone.target.x - drone.position.x)**2 +
-                          (drone.target.y - drone.position.y)**2 +
-                          (drone.target.z - drone.position.z)**2)**0.5
-    velocity = (drone.velocity.x**2 + drone.velocity.y**2 + drone.velocity.z**2)**0.5
-
-    if distance_to_target < 0.5 and velocity < 0.2:
-        return True
-
-    return False
-
-def is_at_yaw(drone):
-    yaw_diff = abs(drone.yaw - drone._setpoint_msg.yaw)
-    print("is_at_yaw :)")
-    print(yaw_diff)
-
-    if yaw_diff < 1:
-        return True
-    return False
-
-# Returnerer jaw mot windmøllen
-def yaw_towards_windmill(drone_pos, windmill_pos):
-    angle = angle3pt((windmill_pos.x, windmill_pos.y + 1), (windmill_pos.x, windmill_pos.y), (drone_pos.x, drone_pos.y))-90
-    yaw = math.radians(angle)
-    return yaw
-
-
-def get_closer_target(drone, target, distance=10):
-    diff = (drone.position.x - target.x, drone.position.y - target.y)
-    lenght = (diff[0]**2 + diff[1]**2)**0.5
-    unit_diff = (diff[0] / lenght, diff[1] / lenght)
-    dx = unit_diff[0] * distance
-    dy = unit_diff[1] * distance
-    return (target.x + dx, target.y + dy)
-
-
-def angle3pt(a, b, c):
-    """Counterclockwise angle in degrees by turning from a to c around b
-        Returns a float between 0.0 and 360.0"""
-    ang = math.degrees(
-        math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
-    return ang + 360.0 if ang < 0.0 else ang
-
-## Returnerer en liste med points(x, y, yaw) med lenght lik total_points. Maa kjøres i første bildeposisjon.
-def points_around_windmill(drone, windmill_pos): # need: import math
-    radius = RADIUS_AROUND_WINDMILL          # Avstand fra fyrtårn [m]
-    total_points = 3    # Antall punkter rundt fyrtårnet. Funker med 1, 2, 3, 4, 6, 12.
-    t = list((range(0, 360, int(360/12))))
-    point = []
-    x = []
-    y = []
-    for i in range(len(t)):  # Lager 12 punkter med radius radius fra origo
-        x.append(radius*(math.sin(math.radians(t[i]))))
-        y.append(radius*(math.cos(math.radians(t[i]))))
-    index_array = list((range(int(12/total_points), 12, int(12/total_points))))  # Første punkt er drone_pos!! (evt endre på første )
-     # Første punkt er drone_pos!!
-     # Finner vinkel for verdenskoordinater til dronekoordinater
-    angle = angle3pt((windmill_pos.x, windmill_pos.y + radius), (windmill_pos.x, windmill_pos.y), (drone.position.x, drone.position.y))
-    point.append(Super_point(drone.position.x, drone.position.y, math.radians(angle-90)))  # Første punkt er drone_pos!!
-    for i in index_array:
-        point.append(Super_point((x[i]*math.cos(math.radians(angle)) - y[i]*math.sin(math.radians(angle))) + windmill_pos.x, (y[i]*math.cos(math.radians(angle)) + x[i]*math.sin(math.radians(angle))) + windmill_pos.y, math.radians(angle3pt((windmill_pos.x, windmill_pos.y + radius), (windmill_pos.x, windmill_pos.y), ((x[i]*math.cos(math.radians(angle)) - y[i]*math.sin(math.radians(angle))) + windmill_pos.x, (y[i]*math.cos(math.radians(angle)) + x[i]*math.sin(math.radians(angle))) + windmill_pos.y))-90)))
-    return point
-
-
-def score(report, score_dict):
-    return score_dict[(report.position.x, report.position.y)]
 
 
 
