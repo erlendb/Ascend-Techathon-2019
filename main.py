ef8d32f258e0a65ef7e62107fb439ca18fd94519
Michael Hoyer <michael.hoyer3@gmail.com>
2019-10-13 05:07:28 +0200

    rotation change

--- main.py
+++ main.py
@@ -68,10 +68,11 @@ class Flying_to_target(smach.State):
 
             next_target = userdata.path[0]
             angle = angle3pt((target.x, target.y), (userdata.drone.position.x, userdata.drone.position.y), (next_target.x, next_target.y))
+            print(angle)
             if math.sin(math.radians(angle)) > 0:
-                userdata.sub_path = points_around_windmill(Super_point(x_new, y_new, 0), userdata.current_windmill, clockwise=False)
-            else:
                 userdata.sub_path = points_around_windmill(Super_point(x_new, y_new, 0), userdata.current_windmill, clockwise=True)
+            else:
+                userdata.sub_path = points_around_windmill(Super_point(x_new, y_new, 0), userdata.current_windmill, clockwise=False)
 
 
         # TODO: collision avoidance
